f18218f4ed97d1292d7aa7666f63a8c026efb1f8
wwj <bbsy789@126.com>
2022-11-05 11:04:04 +0800

    改变了文本的编码，全部应该都是utf-8

--- latexFigureCodeGenerator/main.cpp
+++ latexFigureCodeGenerator/main.cpp
@@ -7,7 +7,7 @@ auto main() -> int
 	using std::string;
 	string name{},format{},path{};
 	int size{},numberPerRow{};
-	cout << "这个小程序用于生成Latex多图片代码 ";
+	cout << "这个小程序用于生成LaTex figure代码 ";
 	cout << "作者：bbsy789\n";
 	cout << "请输入图片文件名:\n";
 	cin >> name;
@@ -23,22 +23,22 @@ auto main() -> int
 		cout << "Error:please input again!\n";
 		cin >> format;
 	}
-	cout << "请输入图片相对路径：\n";
+	cout << "请输入图片相对路径:\n";
 	cin >> path;
 	while (path.size() == 0)
 	{
 		cout << "Error:please input again!\n";
 		cin >> format;
 	}
-	cout << "本程序假定Paraview默认保存的图片是四位数字！从0000开始\n";
-	cout << "请输入图片结束数字（，）：\n";
+	cout << "本程序假定Paraview默认保存的图片是四位数字!从0000开始\n";
+	cout << "请输入图片结束数字\n";
 	cin >> size;
 	while (size == 0)
 	{
 		cout << "错误：输入的图片数量为0，请重新输入";
 		cin >> size;
 	}
-	cout << "请输入每行图片数量：\n";
+	cout << "请输入每行图片数量:\n";
 	cin >> numberPerRow;
 	while (numberPerRow == 0)
 	{
